03a240f80ab221f4a638cba4f5bffdc32d757b47
ZHAOTAO <zhaotao@bd-apaas.com>
2026-07-30 23:02:38 +0800

    fix: localize date and time pickers to Chinese

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import ElementPlus from "element-plus";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
 import "element-plus/dist/index.css";
 import "element-plus/theme-chalk/dark/css-vars.css";
 import "virtual:uno.css";
@@ -10,4 +11,4 @@ import "./styles.css";
 
 document.documentElement.classList.add("dark");
 
-createApp(App).use(pinia).use(router).use(ElementPlus).mount("#app");
+createApp(App).use(pinia).use(router).use(ElementPlus, { locale: zhCn }).mount("#app");
